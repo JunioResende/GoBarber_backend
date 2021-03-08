390b0cc8eaf9f16bba103bb044522fd646c83bf8
JunioResende <junioresende23@gmail.com>
2021-03-08 07:39:23 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import AppError from './Error/AppError';
@@ -9,6 +10,8 @@ import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
